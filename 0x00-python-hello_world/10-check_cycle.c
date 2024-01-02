ae4cfdefaa3f98a38f76dab09bb9145911074dd7
essaket <hichamessaket17gmail.com>
2024-01-02 08:24:07 -0800

    writing task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * check_cycle - a function that check a list fi cycle or not
+ *
+ * @list: a pinter of list
+ *
+ * Return: 1 if list is cycle or 0 if not
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list, *fast = list;
+
+	if(!list)
+		return (0);
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
