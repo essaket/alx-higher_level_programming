63be59ef9f62d6af478f0cebcf5330cc92413cff
essaket <hichamessaket17gmail.com>
2024-01-02 16:29:15 -0800

    writing task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for x in range(ord('z'), ord('a') - 1, -1):
-    y = 0 if x % 2 == 0 else y = 32
+    if x % 2 == 0:
+        y = 0
+    else:
+        y = 32
     print("{}".format(x - y), end="")
