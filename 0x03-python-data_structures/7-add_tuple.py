692ec2d1cbce865d6c3394ef8d5943e447728655
essaket <hichamessaket17gmail.com>
2024-01-07 10:09:59 -0800

    writing task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#1/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    i = tuple_a[0] if len(tuple_a) > 0 else 0
+    j = tuple_a[1] if len(tuple_a) > 1 else 0
+    k = tuple_b[0] if len(tuple_b) > 0 else 0
+    l = tuple_b[1] if len(tuple_b) > 1 else 0
+    return (i + k, j + l)
