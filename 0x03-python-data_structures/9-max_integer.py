73608d289d21672321b8661a06dfc53ff098a5d6
essaket <hichamessaket17gmail.com>
2024-01-07 12:21:21 -0800

    writing task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max = 0
+    for i in range(len(my_list)):
+        max = my_list[i] if my_list[i] >= max
+    return max
