e7d4a7bf785ddd75f5185ba21edbf272eac5338f
essaket <hichamessaket17gmail.com>
2024-01-10 11:55:47 -0800

    writing task 9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_a_dictionary = a.dictionary.copy()
+    for key, value in new_a_dictionary.items():
+        new_a_dictionary[key] = value * 2
+    return new_a_dictionary
