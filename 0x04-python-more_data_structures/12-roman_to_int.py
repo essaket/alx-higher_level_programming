5d8d61370e8c9368536ee8aa2ef3606680dec294
essaket <hichamessaket17gmail.com>
2024-01-10 12:49:01 -0800

    writing task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    n = 0
+    t = 0
+    for i in reversed(roman_string):
+        n = roman[i]
+        t += n if t < n * 5 else -n
+    return t
