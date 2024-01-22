b6ca1bea748b6fb5d5219f369f079bf728720c5c
essaket <hichamessaket17gmail.com>
2024-01-22 06:03:52 -0800

    Writing task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+def safe_print_division(a, b):
+    try:
+        division = a / b
+    except (ZeroDivisionError):
+        division = None
+    finally:
+        print("Inside result: {}".format(div))
+        return division
