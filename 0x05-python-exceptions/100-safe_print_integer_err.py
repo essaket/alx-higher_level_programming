70920bebe9c4487f3016c47138a3785dc90e41bc
essaket <hichamessaket17gmail.com>
2024-01-22 09:54:02 -0800

    Writing task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,10 +2,9 @@
 import sys
 
 def safe_print_integer_err(value):
-    x = True
     try:
         print("{:d}".format(value))
+        x = True
     except Exception as error:
-        print("Exception:", error, file=sys.stderr)
+        print("Exception: {}", error, file=sys.stderr)
         x = False
-    return x
