d47aeeb224ac5c15f25f1257c0fd6924a8f17524
essaket <hichamessaket17gmail.com>
2024-01-23 04:15:01 -0800

    Writing task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """return the size private attribute"""
+        return self.__size
+
+    def size(self, value):
+        """sets the size private attribute value"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns the current square area"""
+        return (self.__size * self.__size)
