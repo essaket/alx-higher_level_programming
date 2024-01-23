0da6378987bab8b857749717ebb9ec154b49c161
essaket <hichamessaket17gmail.com>
2024-01-23 12:36:49 -0800

    Writing task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,12 +5,13 @@
 class Square:
     """Represent a square."""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """Initialize a new Square.
         Args:
             size (int): The size of the new square.
         """
         self.size = size
+        self.position = position
 
     @property
     def size(self):
