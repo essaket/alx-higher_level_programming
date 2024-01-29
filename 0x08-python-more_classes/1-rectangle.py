b2579ce68e81cc6c2cf36d66274c37d46efd51a5
essaket <hichamessaket17gmail.com>
2024-01-29 07:53:02 -0800

    Writing Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,7 +6,6 @@ Write an empty class Rectangle that defines a rectangle
 
 class Rectangle:
     """Defines an empty Rectangle"""
-
     def __init__(self, width=0, height=0):
         """Initialize the Rectangle"""
         self.__width = width
