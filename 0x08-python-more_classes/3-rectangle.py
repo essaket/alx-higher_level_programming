6d47909e5b1eb4c7ea750aa6ac124d378a269c7b
essaket <hichamessaket17gmail.com>
2024-01-29 08:55:47 -0800

    Writing Task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,10 +52,11 @@ class Rectangle:
 
     def __str__(self):
         """retrns a rectangle with the character '#'"""
-        rec_str = ""
-        if self.__width !=0 or self.__height != 0:
-            for i in range(self.__height):
-                for j in range(self.__width):
-                    rec_str += "#"
-                    rec_str += "\n"
-        return rec_str
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        rect = ""
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
