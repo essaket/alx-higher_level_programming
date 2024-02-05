fe38fd3c86a9cb9168b5a12b0572477928664721
essaket <hichamessaket17gmail.com>
2024-02-05 11:50:55 -0800

    Writing Task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A subclass representing a rectangle"""
+    def __init__(self, size):
+        """Initialize"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """return the area of square"""
+        return self.__size ** 2
