1cebbed661bb47197668a835c51581d8ef4080d2
essaket <hichamessaket17gmail.com>
2024-02-05 10:39:08 -0800

    Writing Task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,4 +4,4 @@
 
 def is_kind_of_class(obj, a_class):
     '''Determines if an object is a subclass of a class.'''
-    return isinstance(obj, a_class)
+    return True if isinstance(obj, a_class) else False
