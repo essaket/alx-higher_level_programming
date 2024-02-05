c73b2cbaf017908a92d6e3d1872d4b3a6072348b
essaket <hichamessaket17gmail.com>
2024-02-05 12:36:29 -0800

    Writing Task 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,6 +6,7 @@ class Myint(int):
     """Class inheriting from int,
     But reverses the behavior of != and ==.
     """
+
     def __eq__(self, other):
         """Equality becomes inequality."""
         return super().__ne__(other)
