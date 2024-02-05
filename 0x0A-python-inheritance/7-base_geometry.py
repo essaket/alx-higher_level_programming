e45a56c1b3bca7e834a372e26dfa0670171c05a5
essaket <hichamessaket17gmail.com>
2024-02-05 10:59:54 -0800

    Writing Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """A BaseGeometry class"""
+    def area(self):
+        """raise an exception"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """A method that validates value"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise TypeError("{} must be greater than 0".format(name))
