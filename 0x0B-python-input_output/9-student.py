1a9ffb57e5392d4dc637a0cc484d0acc40c3ec87
essaket <hichamessaket17gmail.com>
2024-02-06 04:27:02 -0800

    Writing Task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,7 +7,7 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """Initialize a new student"""
         self.first_name = first_name
-        self.last-name = last_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
