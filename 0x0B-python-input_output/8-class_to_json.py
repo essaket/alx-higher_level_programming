9ea85d7d7456c1d1285c73422d782224a1e11558
essaket <hichamessaket17gmail.com>
2024-02-06 04:13:36 -0800

    Writing Task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a python class to JSON fuction"""
+
+
+def def class_to_json(obj):
+    """Method returns the dictionary represntation of a simple data structure"""
+    return obj.__dict__
