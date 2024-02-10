bdc29361ef3c508d5aa36a5e86916e269b90c1df
essaket <hichamessaket17gmail.com>
2024-02-10 03:41:00 -0800

    Writing task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Inisialize a new square"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns info string of the square"""
+        return "[Square] ({}) {}/{} - {}".\
+                format(self.id, self.x, self.y, self.width)
+
