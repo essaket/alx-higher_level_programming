c46b65750118615b512d91020dac4a559f2a0fbf
essaket <hichamessaket17gmail.com>
2024-02-12 10:07:13 -0800

    Writing task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,3 +13,13 @@ class Square(Rectangle):
         """Returns info string of the square"""
         return "[Square] ({}) {}/{} - {}".\
             format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """Size of square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
