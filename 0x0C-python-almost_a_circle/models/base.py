dd318ee760ea081dfc277f1a58bd666bac3e3877
essaket <hichamessaket17gmail.com>
2024-02-12 10:36:41 -0800

    Writing task 19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -58,8 +58,11 @@ class Base:
     def load_from_file(cls):
         '''Loads string from file and unjsonifies.'''
         from os import path
-        file = "{}.json".format(cls.__name__)
-        if not path.isfile(file):
-            return []
-        with open(file, "r", encoding="utf-8") as f:
-            return [cls.create(**d) for d in cls.from_json_string(f.read())]
+        l = []
+        list_dicts = []
+        if os.path.exists(cls.__name__ + ".json"):
+            with open(cls.__name__ + ".json", 'r') as file:
+                list_dicts = cls.from_json_string(file.read())
+                for d in list_dicts:
+                    l.append(cls.create(**d))
+        return l
