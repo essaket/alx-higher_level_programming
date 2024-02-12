cb3f94b990c8369b026d0f78512872b4e91799b3
essaket <hichamessaket17gmail.com>
2024-02-12 10:28:08 -0800

    Writing task 19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -56,15 +56,11 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
-        """Returns a list of instances."""
-
-        filename = cls.__name__ + ".json"
-        l = []
-        list_dicts = []
-        if os.path.exists(filename):
-            with open(filename, 'r') as f:
-                s = f.read()
-                list_dicts = cls.from_json_string(s)
-                for d in list_dicts:
-                    l.append(cls.create(**d))
-        return l
+        """Returns a list of instances"""
+    filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
