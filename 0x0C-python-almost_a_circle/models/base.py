27af89aac04b09bc7b46f4a64afbd2f5a05855c9
essaket <hichamessaket17gmail.com>
2024-02-12 09:49:27 -0800

    Writing task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a base model class"""
+import json
+from models.rectangle import Rectangle
+from models.square import Square
+import os
+import csv
+
+
+class Base:
+    """This class will be the “base” of all other classes in this project"""
+
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """Initialize a new Base"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
