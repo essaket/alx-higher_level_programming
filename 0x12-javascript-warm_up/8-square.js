fa7f8b03cb8f7b487f36f9beb33bd6dbcab7eb07
essaket <hichamessaket17gmail.com>
2024-04-08 12:57:37 -0700

    Writing task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const size = parseInt(process.argv[2]);
-if (isNaN(size)) {
+if (isNaN(size) || process.argv[2] === undefined) {
     console.log('Missing size');
 } else {
     for (let i = 0; i < size; i++) {
