b1ae2bb184e8c2373031225ee930339408686893
essaket <hichamessaket17gmail.com>
2024-04-08 13:06:33 -0700

    Writing task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,6 +4,6 @@ if (isNaN(size) || process.argv[2] === undefined) {
     console.log('Missing size');
 } else {
     for (let i = 0; i < size; i++) {
-	console.log('X'.repeat(size));
+	  console.log('X'.repeat(size));
     }
 }
