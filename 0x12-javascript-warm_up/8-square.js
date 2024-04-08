93103b543aec6ec25068c718136f5a883240f98f
essaket <hichamessaket17gmail.com>
2024-04-08 11:48:22 -0700

    Writing task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (isNaN(Number(process.argv[2]);
+  console.log('Missing size');
+} else {
+  let x = 0;
+  while (x < Number(process.argv[2]) {
+     console.log('C is fun');
+     x++;
+  }
+}
