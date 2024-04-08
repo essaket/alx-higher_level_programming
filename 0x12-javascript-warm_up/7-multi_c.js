270f1d04c7eea21fbbe265ab6dcb9df570556d87
essaket <hichamessaket17gmail.com>
2024-04-08 11:31:42 -0700

    Writing task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const times = parseInt(process.argv[2]);
-if (isNaN(times)) {
-    console.log('Missing number of occurrences');
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
 } else {
-   for (let i = 0; i < times; i++) {
-       console.log('C is fun');
-    }
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
