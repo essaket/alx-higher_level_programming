62e9aef2bfab726c34398703b6996036f5f26f7d
essaket <hichamessaket17gmail.com>
2024-04-08 17:46:51 -0700

    Writing task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 function add (a, b) {
-    if (isNaN(a) || isNaN(b)) {
-	return NaN;
-    }
-    return a + b;
+  if (isNaN(a) || isNaN(b)) {
+  return NaN;
+  }
+  return a + b;
 }
+
 console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
