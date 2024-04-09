c25095bd9403104ff819f69539e8ab0d9d760d03
essaket <hichamessaket17gmail.com>
2024-04-08 17:45:15 -0700

    Writing task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,4 +5,4 @@ function add (a, b) {
     }
     return a + b;
 }
-console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
