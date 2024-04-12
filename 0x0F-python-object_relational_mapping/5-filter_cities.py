c6b939a216919bf64f8e838b5d1e20634a31187f
essaket <hichamessaket17gmail.com>
2024-04-12 14:21:16 -0700

    Writing task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    c = db.cursor()
+
+    c.execute("SELECT cities.name FROM cities INNER JOIN states\
+            ON states.id=cities.state_id WHERE states.name=%s", (sys.argv[4],))
+    [print(x) for x in c.fetchall()]
+
+    c.close()
+    db.close()
