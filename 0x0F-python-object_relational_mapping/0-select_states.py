5ebcb30876339dcffd78e90036413a2aa055ce3e
essaket <hichamessaket17gmail.com>
2024-04-12 04:46:06 -0700

    Writing task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,10 +6,10 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
-                        port=3306,
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3])
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
     curs = db.cursor()
     curs.execute("SELECT * FROM states ORDER BY id ASC")
