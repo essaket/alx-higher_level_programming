f5b4f250f621090ae24556ef5e0e7d34ba4ff836
essaket <hichamessaket17gmail.com>
2024-04-13 02:20:26 -0700

    Writing task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""deletes all State objects with a name containing the letter a from the dtb"""
+"""deletes all State objects with a name containing the letter a from dtb"""
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
