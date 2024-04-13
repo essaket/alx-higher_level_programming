712738bc2fb0411873fe144ffa338484352583b8
essaket <hichamessaket17gmail.com>
2024-04-13 02:07:09 -0700

    Writing task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" prints the State object with the name passed as argument from the dtb"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_x = session.query(State).filter_by(id=2).first()
+    new_x.name = 'New Mexico'
+    session.commit()
