d2201eae144ccb9615626b502237e1958cefa8ae
essaket <hichamessaket17gmail.com>
2024-04-13 02:53:18 -0700

    Writing task 15

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Defines a City model and Inherits from SQLAlchemy Base and
-            links to the MySQL table cities."""
+# Defines a City model.
+# Inherits from SQLAlchemy Base and links to the MySQL table cities.
+
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,8 +9,13 @@ Base = declarative_base()
 
 
 class City(Base):
-    """Represents a city for a MySQL database"""
+    """Represents a city for a MySQL database.
 
+    Attributes:
+        id (sqlalchemy.Column): The city's id.
+        name (sqlalchemy.Column): The city's name.
+        state_id (sqlalchemy.Column): The city's state id.
+    """
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
