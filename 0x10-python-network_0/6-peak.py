4528a05abd92095ccd7a8edfa25f6edd887a4e52
essaket <hichamessaket17gmail.com>
2024-04-25 12:59:02 -0700

    Writing task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a peak-finding algorithm"""
+
+
+def find_peak(list_of_integers):
+    """A function that finds a peak in a list of unsorted integers"""
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left < right:
+        m = (left + right) // 2
+        list_of_integers[m] > list_of_integers[m + 1] ? right = m : left = m + 1
+    return list_of_integers[left]
