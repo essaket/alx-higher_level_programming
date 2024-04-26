5106beef63cb92620b42d0a4f9218681a789c162
essaket <hichamessaket17gmail.com>
2024-04-26 11:09:10 -0700

    Writing task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,13 +3,11 @@
 to the passed URL with the email as a parameter, and displays
 the body of the response (decoded in utf-8)"""
 from urllib import request, parse
-import sys
-
+from sys import argv
 
 if __name__ == "__main__":
-    values = {'email': sys.argv[2]}
-    data = parse.urlencode(values)
-    data = data.encode('utf-8')
-    req = request.Request(sys.argv[1], data)
-    with request.urlopen(req) as res:
-        print(res.read().decode('utf-8'))
+    values = {'email': argv[2]}
+    data = parse.urlencode(values).encode('utf-8')
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as x:
+        print(x.read().decode('utf-8'))
