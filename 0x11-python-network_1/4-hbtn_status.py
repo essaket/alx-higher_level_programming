f99f7318867a366a1f88e8bd2acbeb4d4b0b16fc
essaket <hichamessaket17gmail.com>
2024-04-26 15:12:48 -0700

    Writing task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,8 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get(https://intranet.hbtn.io/status)
-    r = requests.get(url)
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
