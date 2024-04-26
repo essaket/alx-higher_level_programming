b1f5f8af83418c15c410521dfe4f4bd429eb4237
essaket <hichamessaket17gmail.com>
2024-04-26 10:56:27 -0700

    Writing task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays
+the body of the response (decoded in utf-8)"""
+from urllib import request, parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    value = {"email": sys.argv[2]}
+    data = parse.urlencode(value).encode("utf-8")
+    request = request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(request) as x:
+        print(x.read().decode("utf-8"))
+
+
