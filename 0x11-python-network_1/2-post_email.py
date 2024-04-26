622481305ecaf2e6c3fa21e284f9ae2420162660
essaket <hichamessaket17gmail.com>
2024-04-26 11:04:23 -0700

    Writing task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""A script that takes in a URL and an email, sends a POST request
-to the passed URL with the email as a parameter, and displays
-the body of the response (decoded in utf-8)"""
+"""A script that takes in a URL and an """
 from urllib import request, parse
 from sys import argv
 
