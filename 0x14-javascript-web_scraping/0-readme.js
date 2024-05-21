fd8c5f07edce6d989c6526570171fa6f42158322
essaket <hichamessaket17@gmail.com>
2024-05-21 09:16:21 -0700

    Writing task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 //Script that reads and prints the content of a file
 
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-	if (err) {
-		console.log(err);
+fs.readFile(process.argv[2], 'utf8', function (error, data) {
+	if (error) {
+		console.log(error);
 	} else {
 		console.log(data);
 	}
